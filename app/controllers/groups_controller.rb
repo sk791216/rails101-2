3ff24cc715fbe460aa7116b1c794bc479ec702d8
meme <sk791216@gmail.com>
2019-03-29 00:52:26 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,22 @@ class GroupsController < ApplicationController
     @group = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path ,notice: "Create success!"
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
