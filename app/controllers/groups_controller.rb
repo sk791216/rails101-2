363d96323c998dd1f89d96c39519a324e001c027
meme <sk791216@gmail.com>
2019-04-01 21:50:09 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,7 +16,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
-      redirect_to goups_path ,notice: "Create success!"
+      redirect_to groups_path ,notice: "Create success!"
     else
       render :new
     end
