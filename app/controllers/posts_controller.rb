e12d42ff02ade461a8bf53ea0827beb8794e6741
meme <sk791216@gmail.com>
2019-04-21 18:39:30 +0800

    implement edti/delete post button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path ,notice: "Update successful"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
